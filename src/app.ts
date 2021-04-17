17e62aa2eba4a3fc5eb76b2dad80f4bb98777dfa
richardnguyen99 <richard.ng0616@gmail.com>
2021-04-17 14:16:36 -0700

    Added static files for application

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ app.set("view engine", "ejs");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", homeController.index);
 
