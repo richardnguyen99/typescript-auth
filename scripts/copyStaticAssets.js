bf8669b28072088495a7084ebb01a475fb748dc7
richardnguyen99 <richard.ng0616@gmail.com>
2021-04-18 13:49:40 -0700

    Added FontAwesome for icons

--- scripts/copyStaticAssets.js
+++ scripts/copyStaticAssets.js
@@ -1,3 +1,5 @@
 const shelljs = require("shelljs");
 
 shelljs.cp("-R", "src/public/ts/lib", "dist/public/js/");
+// shelljs.cp("-R", "src/public/fonts/", "dist/public/");
+// shelljs.cp("-R", "src/public/webfonts/", "dist/public/");
