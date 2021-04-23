f54790d3149765c88d9bf63ddb9aac0e030fd887
Richard Nguyen <richard.ng0616@gmail.com>
2021-04-23 15:26:39 -0700

    Added brand and favicon

--- scripts/copyStaticAssets.js
+++ scripts/copyStaticAssets.js
@@ -1,5 +1,6 @@
 const shelljs = require("shelljs");
 
 shelljs.cp("-R", "src/public/ts/lib", "dist/public/js/");
+shelljs.cp("-R", "src/public/assets", "dist/public/")
 // shelljs.cp("-R", "src/public/fonts/", "dist/public/");
 // shelljs.cp("-R", "src/public/webfonts/", "dist/public/");
