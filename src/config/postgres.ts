8fa26e46a0e5d2584bce2501095a5d0e8ab027b4
unknown <richard.ng0616@gmail.com>
2021-05-02 09:33:10 -0700

    Added DATABASE_URL configuration for production

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -5,5 +5,5 @@ import { Pool, Client } from "pg";
 
 
 
-export const pool = typeof process.env.DATABASE_URL ? new Pool({ connectionString: process.env.DATABASE_URL }) : new Pool();
-export const client= typeof process.env.DATABASE_URL ? new Client({ connectionString: process.env.DATABASE_URL }) : new Client();
+export const pool = typeof process.env.DATABASE_URL === "undefined" ? new Pool({ connectionString: process.env.DATABASE_URL }) : new Pool();
+export const client= typeof process.env.DATABASE_URL === "undefined" ? new Client({ connectionString: process.env.DATABASE_URL }) : new Client();
